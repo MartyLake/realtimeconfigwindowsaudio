98289fc514f1966ea169ceb8d83289eda3883234
MartyLake <matthieu@talbot.audio>
2019-05-14 22:41:23 +0200

    Add template files

---
+++ template.cmd
@@ -0,0 +1,8 @@
+REM from http://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+@ECHO OFF
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath=%ThisScriptsDirectory%MyPowerShellScript.ps1
+REM non admin call
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
+REM admin call
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
---
+++ template.ps1
@@ -0,0 +1,8 @@
+# From http://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
+# If running in the console, wait for input before closing.
+if ($Host.Name -eq "ConsoleHost")
+{ 
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.FlushInputBuffer()   # Make sure buffered input doesn't "press a key" and skip the ReadKey().
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
